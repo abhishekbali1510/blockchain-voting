b7bedf99614cbc023db6ca38f793b5435b4bce51
abhishekbali1510 <abhishekbali15oct@gmail.com>
2022-06-20 12:13:06 +0530

    temp changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,20 @@
 import "./App.css";
-import {Connection} from "./components/Connection";
-import Reterive from "./components/reterive";
-import Store from "./components/store";
+// import {Connection} from "./components/Connection";
+// import Reterive from "./components/reterive";
+// import Store from "./components/store";
+import VoterRegistration from './components/VoterRegistration';
+import CandidateRegistration from './components/CandidateRegistration';
 
 function App() {
   return (
     <>
-    <h1>hello App</h1>
-    <Connection />
+    {/* <h1>hello App</h1> */}
+    {/* <Connection />
     
     <Store />
-    <Reterive />
+    <Reterive /> */}
+  <VoterRegistration></VoterRegistration>
+  <CandidateRegistration></CandidateRegistration>
     </>
   );
 }
